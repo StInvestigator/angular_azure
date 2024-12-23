9f44c7ed7c4f314081ecd07e86965a7fd214350c
Murashko Vladislav <vladislavuh2000@gmail.com>
2024-12-23 21:29:38 +0200

    Add project files.

---
+++ angular_azure.Server/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace angular_azure.Server
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
